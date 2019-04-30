3dd9979f6ddc633e1c250727a1d0e817182054f1
lucas-chaves <lucas.limachaves@outlook.com>
2019-04-29 23:28:13 -0300

    Graphql-Intro

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import { getFirstName } from './user'
+import {GraphQLServer} from 'graphql-yoga';
 
-console.log(getFirstName('Lucas fedido '))
+const typeDefs = `
+    type Query{
+        lucas: User!
+
+    }
+    type User{
+        nome: String!
+        email: String!
+        idade: Int!
+    }
+    `
+
+const resolvers = {
+    Query: {
+        lucas(){
+            return {nome: "Lucas 4h50",email: "Upaltasaventuras@gmail.com", idade: 25}
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+});
+
+server.start(()=>{
+    console.log("up altas aventuras")
+});
