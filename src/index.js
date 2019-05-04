11bb670819b21c609a70da7b02c1b918bab50924
lucas-chaves <lucas.limachaves@outlook.com>
2019-05-04 13:26:06 -0300

    Grafo dos comentarios

--- src/index.js
+++ src/index.js
@@ -26,16 +26,19 @@ const rates = [
     id: 11,
     titulo: 'Achei legal',
     avaliacao: 5,
+    autor: 1,
   },
   {
     id: 12,
     titulo: 'Achei legal',
     avaliacao: 5,
+    autor: 2,
   },
   {
     id: 13,
     titulo: 'Achei legal',
     avaliacao: 5,
+    autor: 3,
   },
 ]
 
@@ -43,20 +46,26 @@ const comentarios = [
   {
     id: 101,
     texto: 'Podia ser melhor',
+    autor: 1,
+    avaliacao: 11,
   },
   {
     id: 102,
     texto: 'Podia ser melhor',
+    autor: 2,
+    avaliacao: 11,
   },
   {
     id: 103,
     texto: 'Podia ser melhor',
+    autor: 1,
+    avaliacao: 12,
   },
 ]
 
 const typeDefs = `
     type Query{
-      lucas: User!
+      usuario: User!
       avaliation: Avaliacao!
       comentario: Comentario!
       usuarios: [User!]!
@@ -69,22 +78,29 @@ const typeDefs = `
       nome: String!
       email: String!
       idade: Int!
+      avaliacoes: [Avaliacao!]!
+      comentarios: [Comentario!]!
     }
     type Avaliacao{
       id: ID!
       titulo: String!
       avaliacao: Int!
+      autor: User!
+      comentarios: [Comentario!]!
     }
     type Comentario{
       id:ID!
       texto: String!
+      autor: User!
+      avaliacao: Avaliacao!
     }
     `
 
 const resolvers = {
   Query: {
-    lucas() {
+    usuario() {
       return {
+        id: 4,
         nome: 'Lucas 4h50',
         email: 'Upaltasaventuras@gmail.com',
         idade: 25,
@@ -109,6 +125,29 @@ const resolvers = {
     },
     comentarios(parent, args, ctx, info) {
       return comentarios
+    },
+  },
+  Avaliacao: {
+    autor(parent, args, ctx, info) {
+      return usuarios.find((usuario) => {
+        return usuario.id === parent.autor
+      })
+    },
+    comentarios(parent, args, ctx, info) {
+      return comentarios.find((comments)=>{
+        return comments.autor === parent.id 
+      })
+  },
+  User: {
+    avaliacoes(parent, args, ctx, info) {
+      return rates.find((avaliacao)=>{
+        return avaliacao.autor === parent.id
+      })
+    },
+    comentarios(parent, args, ctx, info) {
+      return comentarios.find((comments)=>{
+        return comments.autor === parent.id
+      })
     },
   },
 }
